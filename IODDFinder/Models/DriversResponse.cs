0f01e8c6f6a82766b13022a7aeddaf3085ab076c
Andrew Sorochak <andrii.sorochak@contrinex.com>
2024-08-22 10:55:48 +0200

    try binding fix on release mode

--- IODDFinder/Models/DriversResponse.cs
+++ IODDFinder/Models/DriversResponse.cs
@@ -2,11 +2,11 @@
 
 public class DriversResponse
 {
-    public List<Content> Content { get; set; }
+    public List<Content> Content { get; set; } = new List<Content>();
     public int Number { get; set; }
     public int Size { get; set; }
     public int NumberOfElements { get; set; }
-    public List<object> Sort { get; set; }
+    public List<object> Sort { get; set; } = new List<object>();
     public bool First { get; set; }
     public bool Last { get; set; }
     public int TotalPages { get; set; }
@@ -17,28 +17,28 @@ public class Content
 {
     public bool HasMoreVersions { get; set; }
     public int DeviceId { get; set; }
-    public string IoLinkRev { get; set; }
-    public string VersionString { get; set; }
+    public string IoLinkRev { get; set; } = "";
+    public string VersionString { get; set; } = "";
     public int IoddId { get; set; }
-    public string ProductId { get; set; }
+    public string ProductId { get; set; } = "";
     public int ProductVariantId { get; set; }
-    public string ProductName { get; set; }
-    public string VendorName { get; set; }
-    public object UploadDate { get; set; }
+    public string ProductName { get; set; } = "";
+    public string VendorName { get; set; } = "";
+    public object UploadDate { get; set; } = "";
     public int VendorId { get; set; }
-    public string IoddStatus { get; set; }
-    public string IndicationOfSource { get; set; }
+    public string IoddStatus { get; set; } = "";
+    public string IndicationOfSource { get; set; } = "";
     public bool HasMd { get; set; }
-    public string DriverName { get; set; }
-    public Md Md { get; set; }
+    public string DriverName { get; set; } = "";
+    public Md? Md { get; set; }
     public string Icon => $"https://ioddfinder.io-link.com/api/productvariants/{ProductVariantId}/files/icon";
 }
 
 public class Md
 {
     public int Id { get; set; }
-    public string FileName { get; set; }
-    public string Revision { get; set; }
-    public object ReleasedAt { get; set; }
-    public object UpdatedAt { get; set; }
+    public string? FileName { get; set; }
+    public string? Revision { get; set; }
+    public object? ReleasedAt { get; set; }
+    public object? UpdatedAt { get; set; }
 }
